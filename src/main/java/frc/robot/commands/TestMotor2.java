4c3778230a2913bf15e8ffd4690547e2388c1e7c
Thor Smith <thorsmith777@gmail.com>
2020-10-02 13:39:21 -0400

    adds python graphing app using networktables
    This change adds a python graphing app using a
    python networktables library. Graphing is done
    with matplotlib.
    This change also fixes a problem for TestMotor2
    command to test if inputSpeed is greater than 1
    as well as equal.

--- src/main/java/frc/robot/commands/TestMotor2.java
+++ src/main/java/frc/robot/commands/TestMotor2.java
@@ -61,7 +61,7 @@ public class TestMotor2 extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    boolean finished = inputSpeed == 1.0;
+    boolean finished = inputSpeed >= 1.0;
     return finished;
   }
 }
